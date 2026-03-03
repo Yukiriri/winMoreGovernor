0c18bf3ec30297c64fb2b5f6db00dd259f7548d9
Yukiriri <50292962+Yukiriri@users.noreply.github.com>
2026-03-04 04:53:40 +0800

    改进实现

--- bin/set-cpufreq-conservative-75.bat
+++ bin/set-cpufreq-conservative-75.bat
@@ -14,8 +14,8 @@ for %%i in (SetAcValueIndex SetDcValueIndex) do (
 
     powercfg -%%i Scheme_Current Sub_Processor PerfBoostMode 1
     powercfg -%%i Scheme_Current Sub_Processor PerfBoostPol 0
-    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold 80
-    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold1 80
+    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold 66
+    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold1 66
     powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold 20
     powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold1 20
     powercfg -%%i Scheme_Current Sub_Processor PerfIncPol 1
